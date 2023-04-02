d2596cc58fa001a2ff3e0c2345127f649f4c625c
Sol <sol.gomez@mi.unc.edu.ar>
2023-04-02 02:16:37 -0700

    de todo un poco

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
 const Home = () => {
 
-	const [medico, setMedico] = useState({})
-	const [farmacia, setFarmacia] = useState({})
+	const [userType, setUserType] = useState({ user: "" })
+	const navigate = useNavigate();
 
-	const handleMedico = () => {
-		
+	const handleClick = (event) => {
+		setUserType({ user: event.target.value })
+		navigate('/login')
 	}
 
 	return (
-		<div className="container">
-			<h1>MedChain</h1>
-			<button>Soy Médico</button>
-			<button>Soy Farmacia</button>
+		<div className="w-full h-96 flex flex-col justify-center items-center gap-5">
+			<button onClick={handleClick} value="doctor" className="rounded text-3xl bg-button w-96 p-5 hover:bg-lime-500">Soy Médico</button>
+			<button onClick={handleClick} value="pharmacy" className="rounded text-3xl bg-button w-96 p-5 hover:bg-lime-500">Soy Farmacia</button>
 		</div>
 	)
 };
