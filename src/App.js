f17ec662eafd9e30fa3af96e827a14ee6c4c860b
Igna Luna <ignaluna98pb@gmail.com>
2023-04-02 13:17:02 -0300

    Intento de wallet connect 1

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import Home from "./components/Home";
 import Form_med from "./components/form_med";
 import Login from "./components/Login";
 import MedChain from "./components/MedChain";
+// import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+// import { Web3Modal } from '@web3modal/react'
+// import { configureChains, createClient, WagmiConfig } from 'wagmi'
+// import { polygonMumbai } from 'wagmi/chains'
+
+// const chains = [polygonMumbai];
+
+// const projectId = String(process.env.REACT_APP_WC_PROJECT_ID)
+// const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+// const wagmiClient = createClient({
+// 	autoConnect: true,
+// 	connectors: w3mConnectors({ projectId, version: 1, chains }),
+// 	provider
+// })
+// const ethereumClient = new EthereumClient(wagmiClient, chains)
+
 
 function App() {
 	return (
 		<>
-			<MedChain/>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/form' element={<Form_med />} />
-				<Route path='/login' element={<Login />} />
-			</Routes>
+			{/* <WagmiConfig client={wagmiClient}> */}
+				<MedChain />
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/form' element={<Form_med />} />
+					<Route path='/login' element={<Login />} />
+				</Routes>
+
+			{/* </WagmiConfig>
+
+			<Web3Modal projectId={projectId} ethereumClient={ethereumClient} /> */}
 		</>
 	);
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Web3Button } from '@web3modal/react'
 
 const Login = () => {
 
-    return(
+    return (
         <div className="w-full h-96 flex flex-col justify-center items-center gap-5">
             <button className="rounded text-3xl bg-button w-96 p-5 hover:bg-lime-500">Conectar Wallet</button>
+            {/* <Web3Button /> */}
         </div>
     )
 }
