a1f82b07a8f9026c65fbee93e148caead0df322a
Sol <sol.gomez@mi.unc.edu.ar>
2023-05-10 20:46:46 -0700

    estilos fondo, navbar y form

--- src/components/MedChain.jsx
+++ src/components/MedChain.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const MedChain = () => {
 
 	return (
-		<div className="flex justify-center m-2 p-2 pb-1">
-			<h1 className="text-med text-7xl text-extrabold font-Manrope">Med</h1>
-            <h1 className="text-chain text-7xl text-extrabold font-Manrope">Chain</h1>
+		<div className="flex justify-start items-center w-full h-16 px-4 text-white bg-navBar">
+			<h1 className="text-med text-4xl text-extrabold font-Manrope">Med</h1>
+            <h1 className="text-chain text-4xl text-extrabold font-Manrope">Chain</h1>
 		</div>
 	)
 };
