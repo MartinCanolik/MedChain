709311dd41ab455ac11c7f12433c625280f1b033
Igna Luna <ignaluna98pb@gmail.com>
2023-05-12 17:17:56 -0300

    Renderiza y linkea cards con las droguitas ;D

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from './CSS/Card.module.css'
 import img from '../logos/example-logo.jpg'
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 
 const GET_PRESCRIPTIONS = gql`
@@ -20,38 +20,18 @@ query GetPrescriptions{
 }
 `;
 
-export default function Card ({image}){
-
-    const Search = () => {
-        const[searchTerm, setSearchTerm] = useState('');
-        const { loading, error, data } = useQuery(GET_PRESCRIPTIONS);
-    }
-
-
-
-
-          data.allPrescriptions.map((recipe) => {
-          console.log(recipe)
-          return(
-            <li key={recipe.id}>
-                <h3 className={style.ti}> Dr.{recipe.doctorName} </h3>
-                <p>{recipe.medicineName}</p>
-                <h4 className={style.types}>VTO de receta: {recipe.expiryDate}</h4>
-            </li>)
-        }
-      )
-
-    return (
-        <div key={id} className={style.card}>
-        <div className={style.cd}>
-        <img className={style.carding} src={image? image: img} alt="img not found" width='200px' height='250px'/>
-        <h3 className={style.ti}> Dr.{recipe.doctorName} </h3>
-        <p>{recipe.medicineName}</p>
-        <h4 className={style.types}>VTO de receta: {recipe.expiryDate}</h4>
-        
-        {/* <h4 className={style.types}>: {frequency}</h4> */}
-        </div>
-        </div>
-
+export default function Card({ image, recipe}) {
+  console.log(recipe)
+  return (
+        <li key={recipe.id} className={style.card}>
+          <div className={style.cd}>
+            <img className={style.carding} src={image ? image : img} alt="img not found" width='200px' height='250px' />
+            <h3 className={style.ti}> Dr.{recipe.doctorName} </h3>
+            <p>{recipe.medicineName}</p>
+            <h4 className={style.types}>VTO de receta: {recipe.expiryDate}</h4>
+            <button>Entregado</button>
+          </div>
+        </li>
+    
     )
 }
